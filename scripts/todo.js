3f3da40569aef887dfb0bf5b5f1dcbe635860dbd
CHRIS <NWAKAUC1@GMAIL.COM>
2021-10-19 09:57:51 +0200

    loops

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,4 +1,20 @@
-const todoList = [];
+const todoList = ['eat', 'wash'];
+renderList();
+
+
+function renderList(){
+let todoListHtml = '';
+for (let i = 0; i < todoList.length; i++){
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`
+  todoListHtml += html;
+}
+//console.log(todoListHtml);
+
+document.querySelector('.js-todo-list').innerHTML = todoListHtml;
+
+};
+
 
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
@@ -7,4 +23,6 @@ function addTodo(){
   console.log(todoList);
 
   inputElement.value = '';
+
+  renderList();
 }
